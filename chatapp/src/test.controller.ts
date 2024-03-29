195b625386db0b60c94dec58b836fd7050613c36
Lucien Van Bussel <lvan-bus@f1r6s8.codam.nl>
2024-03-29 11:38:15 +0100

    able to send messages to front end of the requested channel

--- chatapp/src/test.controller.ts
+++ chatapp/src/test.controller.ts
@@ -1,17 +1,41 @@
 import { Controller, Get, Req } from '@nestjs/common'
 import { Request } from 'express'
 
+export class channel {
+	Name: string;
+	Messages: string[];
+
+	constructor(name: string, messages: string[]) {
+		this.Name = name;
+		this.Messages = messages;
+	}
+}
+
 @Controller('test')
 export class TestController {
-    @Get()
+	
+	private channelMap: { [key: string]: channel } = {};
+	
+	constructor() {
+		this.channelMap['lvan-bus'] = new channel('lvan-bus', ['message1', 'message2']);
+		this.channelMap['sbos'] = new channel('sbos', ['message3']);
+	}
+	
+	@Get()
     findAll(@Req() request: Request): string {
         const   queryParam = request.query;
-        const   param = request.query['chatName'];
+        const   param = request.query['chatName'] as string;
 
         console.log("All query params:", queryParam);
         console.log("value of param:", param);
 
-        return JSON.stringify(queryParam);
+		if (this.channelMap[param]) {
+			console.log("query matches");
+        	return JSON.stringify(this.channelMap[param].Messages);
+		}
+		else
+			console.log("query does not match");
+		return 'Query not found'
     }
 
 }
